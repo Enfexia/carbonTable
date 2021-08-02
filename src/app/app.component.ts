56e61070432792bcb50fe39db0449399ee7a10e7
Cem Cevik <cemcevik@uni-bremen.de>
2021-08-02 02:47:36 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  TableHeaderItem,
+  TableItem,
+  TableModel,
+  TableRowSize,
+} from 'carbon-components-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.model.data = [
+      [new TableItem({ data: 'Name 1' }), new TableItem({ data: 'qwer' })],
+      [new TableItem({ data: 'Name 3' }), new TableItem({ data: 'zwer' })],
+      [new TableItem({ data: 'Name 2' }), new TableItem({ data: 'swer' })],
+      [new TableItem({ data: 'Name 4' }), new TableItem({ data: 'twer' })],
+    ];
+    this.model.header = [
+      new TableHeaderItem({ data: 'Name' }),
+      new TableHeaderItem({ data: 'Place' }),
+    ];
+  }
   title = 'tableTest';
+  model = new TableModel();
+  size: TableRowSize = 'md';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  UIShellModule,
+  IconModule,
+  SearchModule,
+  DialogModule,
+  NFormsModule,
+  ButtonModule,
+  TableModule,
+} from 'carbon-components-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UIShellModule,
+    IconModule,
+    SearchModule,
+    DialogModule,
+    NFormsModule,
+    ButtonModule,
+    TableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
